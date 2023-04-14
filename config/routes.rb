b788c611fd536371295fc21907972e44cdec073f
Verity Collins <verity.carlos@gmail.com>
2023-04-14 11:05:41 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? } 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? } 
   
   resources :notes, only: [:index, :create ]
   resources :recommendations, only: [:index, :show, :create, :update, :destroy]
@@ -28,6 +28,6 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? } 
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? } 
   
 end
